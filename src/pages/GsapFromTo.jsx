3e22fe8786239549ace1d4194dc15e31596bc96e
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-03-30 16:24:59 +0530

    Completed

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,23 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+  useGSAP(()=>{
+    gsap.fromTo('#red-box', {
+      x: 0,
+      rotation: 0,
+      borderRadius:'0%',
+    },{
+      x: 300,
+      repeat: -1,
+      borderRadius: '100%',
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: 'bounce.out',
+    })
+  }, [])
 
   return (
     <main>
--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -1,5 +1,21 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapStagger = () => {
   // TODO: Implement the gsap.stagger() method
+  useGSAP(()=>{
+    gsap.to('.stagger-box',{
+      y:100,
+      roation: 360,
+      borderRadius:'100%',
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+      stagger: {
+        amount: 0.5,
+      },
+    })
+  },[])
 
   return (
     <main>
